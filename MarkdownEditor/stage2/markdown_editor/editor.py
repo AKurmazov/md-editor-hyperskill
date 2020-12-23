b8a995196b3c9aa1eef1358dc7479afd479819ff
Alexander Kurmazov <akurmazov2001@gmail.com>
2020-12-23 17:25:54 +0400

    Add stage2 tests

---
+++ MarkdownEditor/stage2/markdown_editor/editor.py
@@ -0,0 +1,53 @@
+FORMATTING_TYPES = ['plain', 'bold', 'italic', 'header', 'ordered-list', 'unordered-list', 'link', 'inline-code', 'new-line']
+SPECIAL_COMMANDS = ['!help', '!done']
+MD = ""
+
+
+def help():
+    print('Available formatting types: ', ' '.join(FORMATTING_TYPES))
+    print('Special commands: ', ' '.join(SPECIAL_COMMANDS))
+
+
+def done():
+    exit()
+
+
+def query_input(message):
+    global MD
+
+    ftype = input(message)
+    if ftype in FORMATTING_TYPES:
+        if ftype == 'plain':
+            pass
+        elif ftype == 'italic':
+            pass
+        elif ftype == 'bold':
+            pass
+        elif ftype == 'inline-code':
+            pass
+        elif ftype == 'header':
+            pass
+        elif ftype == 'ordered-list':
+            pass
+        elif ftype == 'unordered-list':
+            pass
+        elif ftype == 'link':
+            pass
+        elif ftype == 'new-line':
+            pass
+    elif ftype in SPECIAL_COMMANDS:
+        if ftype == '!help':
+            help()
+        elif ftype == '!done':
+            done()
+    else:
+        print('Unknown formatting type or command. Please try again')
+
+    query_input(message)
+
+
+def main():
+    query_input("- Choose a formatter: > ")
+
+
+main()
--- MarkdownEditor/stage2/tests.py
+++ MarkdownEditor/stage2/tests.py
@@ -5,34 +5,48 @@ from hstest.check_result import CheckResult
 
 class SumTest(StageTest):
 
-    answer = [
-
+    answers = [
+        [
+            '- Choose a formatter: > Unknown formatting type or command. Please try again',
+            '- Choose a formatter: > Available formatting types:  plain bold italic header ordered-list unordered-list link inline-code new-line',
+            'Special commands:  !help !done',
+            '- Choose a formatter: > ' * 3
+        ],
+        [
+            '- Choose a formatter: > ' * 9
+        ],
+        [
+            '- Choose a formatter: > '
+        ]
     ]
 
     def generate(self):
         return [
             TestCase(
-                stdin=[],
+                stdin=['nan', '!help', 'header', 'ordered-list', '!done'],
                 attach=0
             ),
             TestCase(
-                stdin=[],
+                stdin=['plain', 'bold', 'italic', 'inline-code', 'link',
+                       'header', 'unordered-list', 'ordered-list', 'new-line', '!done'],
                 attach=1
             ),
             TestCase(
-                stdin=[],
+                stdin=['!done'],
                 attach=2
             ),
         ]
 
     def check(self, reply, attach):
+        print(reply)
+
         reply = reply.strip().split('\n')
-        if len(reply) != len(self.answer[attach]):
-            CheckResult.wrong("Wrong answer. Please try again")
+        if len(reply) != len(self.answers[attach]):
+            CheckResult.wrong('Wrong answer. Please try again')
 
         for i, line in enumerate(reply):
-            if line != self.answer[attach][i]:
-                CheckResult.wrong("Wrong answer. Please try again")
+            if line != self.answers[attach][i]:
+                CheckResult.wrong('Wrong answer. Please try again')
 
         return CheckResult.correct()
 
