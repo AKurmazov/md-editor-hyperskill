624da2869cf60e5f4486a06ba8936725a42e6416
Alexander Kurmazov <akurmazov2001@gmail.com>
2020-12-23 17:01:39 +0400

    Add tasks' source files

---
+++ MarkdownEditor/stage1/markdown_editor/editor.py
@@ -0,0 +1,11 @@
+answer = [
+    "# John Lennon",
+    "or **John Winston Ono Lennon**, was one of *The Beatles*.",
+    "Here are the songs he wrote I like the most:",
+    "* Imagine",
+    "* Norwegian Wood",
+    "* In My Life",
+    "* ~~Hey Jude~~ (that was *McCartney*)"
+]
+
+print(*answer, sep='\n')
