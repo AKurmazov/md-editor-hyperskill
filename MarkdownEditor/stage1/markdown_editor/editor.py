8dfa52c221da92ecd71e3553566bef1897f2ef07
Alexander Kurmazov <akurmazov2001@gmail.com>
2021-03-14 16:20:37 +0300

    Fix stage1

--- MarkdownEditor/stage1/markdown_editor/editor.py
+++ MarkdownEditor/stage1/markdown_editor/editor.py
@@ -1,11 +1,12 @@
 answer = [
-    "# John Lennon",
-    "or **John Winston Ono Lennon**, was one of *The Beatles*.",
-    "Here are the songs he wrote I like the most:",
-    "* Imagine",
-    "* Norwegian Wood",
-    "* In My Life",
-    "* ~~Hey Jude~~ (that was *McCartney*)"
-]
+        "# John Lennon",
+        "or ***John Winston Ono Lennon***, was one of *The Beatles*.",
+        "Here are the songs he wrote I like the most:",
+        "* Imagine",
+        "* Norwegian Wood",
+        "* Come Together",
+        "* In My Life",
+        "* ~~Hey Jude~~ (that was *McCartney*)"
+    ]
 
 print(*answer, sep='\n')
--- MarkdownEditor/stage1/tests.py
+++ MarkdownEditor/stage1/tests.py
@@ -7,10 +7,11 @@ class SumTest(StageTest):
 
     answer = [
         "# John Lennon",
-        "or **John Winston Ono Lennon**, was one of *The Beatles*.",
+        "or ***John Winston Ono Lennon***, was one of *The Beatles*.",
         "Here are the songs he wrote I like the most:",
         "* Imagine",
         "* Norwegian Wood",
+        "* Come Together",
         "* In My Life",
         "* ~~Hey Jude~~ (that was *McCartney*)"
     ]
